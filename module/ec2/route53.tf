7db622563e23e871c5487bf4381ed1f9dcc0bfbe
ajitnitr <92538695+ajitnitr@users.noreply.github.com>
2022-01-28 09:35:17 +0530

    Update route53.tf

--- module/ec2/route53.tf
+++ module/ec2/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "private-record" {
   zone_id = var.PRIVATE_HOSTED_ZONE_ID
-  name    = "${var.DB_COMPONENT}-${var.ENV}.roboshop.internal"
+  name    = "${var.DB_COMPONENT}-${var.ENV}.ajitdevops-internal"
   type    = "A"
   ttl     = "300"
   records = [aws_spot_instance_request.db.private_ip]
